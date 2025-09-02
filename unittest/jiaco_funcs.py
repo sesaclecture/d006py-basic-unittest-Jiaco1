10ec08e28510afab21b1baf86ad9f76025456cac
jiaco <25jiaco@gmail.com>
2025-09-02 17:24:00 +0900

    unittest

---
+++ unittest/jiaco_funcs.py
@@ -0,0 +1,13 @@
+def even_num(n):
+    return "even" if (n) % 2 == 0 else "odd"
+
+def average(nums):
+    return sum(nums) / len(nums)
+
+
+def find_max(nums):
+    return max(nums)
+
+
+def find_min(nums):
+    return min(nums)
--- unittest/test_funcs.py
+++ unittest/test_funcs.py
@@ -1,6 +1,24 @@
 # TODO: 사용자 모듈 import
-
+from jiaco_funcs import even_num, average, find_max, find_min
 
 # TODO: 아래의 코드를 삭제하고 unittest를 작성하세요.
-def test_always():
-    assert False, "Remove me!"
+
+def test_even_num():
+    assert "even" == even_num(4)
+    assert "even" == even_num(14)
+    assert "odd" == even_num(3)
+    assert "odd" == even_num(7)
+
+
+def test_average():
+    assert 3 == average([1, 2, 3, 4, 5])
+    assert 5 == average([1,3,5,7,9])
+        
+
+def test_find_max():
+    assert 4 == (find_max([1, 2, 3, 4]))
+    assert-2 == (find_max([-5, -2, -10]))
+
+def test_find_min():
+    assert 1 == (find_min([1, 2, 3, 4]))
+    assert -10 == (find_min([-5, -2, -10]))
